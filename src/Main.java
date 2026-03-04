10b075a49fdcc8463f0ae8f9a5a9a142b6e8a80e
Luca Tsiklauri <ltsik@myuax.com>
2026-03-04 10:00:18 +0100

    feat (Main): creación del Metodo pedirNumeroPeleadores para que el usuario pueda elegir un valor positivo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
 
+    }
+
+    public static int pedirNumeroPeleadores(int cantidadMaxPeleadores) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de peleadores: ");
+        cantidadMaxPeleadores = sc.nextInt();
+
+        return cantidadMaxPeleadores;
     }
 }
